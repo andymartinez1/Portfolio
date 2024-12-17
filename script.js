8955cbd5f87c3892b8b52a35c228439e8c346713
Andrew Martinez <andy72391@gmail.com>
2024-12-17 00:10:45 -0500

    Changed the font family. Added more media queries to make the site more responsive.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// HAMBURGER MENU
 const navLinks = document.querySelector(".navbar-links");
 const hamburger = document.querySelector(".hamburger");
 
@@ -12,3 +13,43 @@ document.querySelectorAll(".navbar-list-item").forEach((n) =>
     navLinks.classList.remove("active");
   })
 );
+
+// PROJECTS SLIDER
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
+
+// SCROLL TO TOP BUTTON
+const scrollToTop = document.querySelector(".scroll-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    scrollToTop.classList.add("active");
+  } else {
+    scrollToTop.classList.remove("active");
+  }
+}
+
+scrollToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+  });
+});
